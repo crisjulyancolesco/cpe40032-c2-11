631ae961993d9e772d3241bcfded80829a711f53
crisjulyancolesco <crisjulyancolesco@iskolarngbayan.pup.edu.ph>
2022-12-02 17:59:41 +0800

    Challenge 2 - Team 11 - Play Fetch

---
+++ Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+    // Destroy the ball when it hits the dog
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Challenge 2/Scripts/MoveForwardX.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForwardX : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // The ball and the dog to move
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
